911ed0dabae4ae2016ae7226aec4c7e4c37f024a
BeaDella <biadellato@gmail.com>
2024-05-29 10:31:08 -0300

    Otimizando o código

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,51 +1,32 @@
 let carrinho = [];
-let itensCarrinho = document.querySelector('.carrinho__produtos__produto');
+let itensCarrinho = document.getElementById('lista-produtos');
 let resultado = document.getElementById('valor-total');
-let produtoSelecionado = document.querySelector('#produto');
-let quantidade = document.querySelector('input');
 limpar();
 
 function adicionar() {
 
-    let nomeProduto;
-    let valorProduto;
-    let valorTotal;
+    let produtoSelecionado = document.getElementById('produto').value;
+    let quantidade = document.getElementById('quantidade');
+    let nomeProduto = produtoSelecionado.split('-')[0];
+    let valorProduto = produtoSelecionado.split('R$')[1];
     let total = 0;
 
-    if (produtoSelecionado.value == 'Fone de ouvido - R$100') {
-        nomeProduto = 'Fone de ouvido';
-        valorProduto = 100;
-    }
-    else if (produtoSelecionado.value == 'Celular - R$1400') {
-        nomeProduto = 'Celular';
-        valorProduto = 1400;
-    }
-    else if (produtoSelecionado.value == 'Óculos VR - R$5000') {
-        nomeProduto = 'Óculos VR';
-        valorProduto = 5000;
-    }
+    let subtotal = quantidade.value * valorProduto;
+    carrinho.push(subtotal);
 
-    valorTotal = quantidade.value * valorProduto;
-    carrinho.push(valorTotal);
-
-    if (itensCarrinho.innerHTML == '<section class="carrinho__produtos__produto"></section>') {
-        itensCarrinho.innerHTML = itensCarrinho.innerHTML + `<span class="texto-azul">${quantidade.value}x
-    </span> ${nomeProduto} <span class="texto-azul">R$${valorProduto}</span>`;
-    }
-    else {
-        itensCarrinho.innerHTML = itensCarrinho.innerHTML + '<br>' + `<span class="texto-azul">${quantidade.value}x
-    </span> ${nomeProduto} <span class="texto-azul">R$${valorProduto}</span>`;
-    }
+    itensCarrinho.innerHTML = itensCarrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade.value}x</span> ${nomeProduto} <span class="texto-azul">
+    R$${valorProduto}</span></section>`;
     
     for (let i = 0; i < carrinho.length; i++) {
         total += carrinho[i];
     }
-    resultado.textContent = `R$${total}`;
+    resultado.textContent = `R$ ${total}`;
     quantidade.value = '';
 }
 
 function limpar() {
     carrinho = [];
-    itensCarrinho.innerHTML = '<section class="carrinho__produtos__produto"></section>';
-    resultado.textContent = 'R$0';
+    itensCarrinho.innerHTML = '';
+    resultado.textContent = 'R$ 0';
 }
