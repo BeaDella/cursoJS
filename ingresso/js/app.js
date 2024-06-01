6c233f27c57e3b510561433ba6956f45b5d20809
BeaDella <biadellato@gmail.com>
2024-06-01 00:32:47 -0300

    Implementando validação da quantidade inserida

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -3,21 +3,32 @@
     let ingresso = document.getElementById('tipo-ingresso').value;
     let quantidade = document.getElementById('qtd').value;
     let quantidadeIngresso = parseInt(document.getElementById(`qtd-${ingresso}`).textContent);
-    let mensagem;
 
+    let mensagem;
     if (ingresso == 'pista') {
         mensagem = `Quantidade indisponível para o ingresso do tipo ${ingresso}!`;
-    } else {
+    }
+    else {
         mensagem = `Quantidade indisponível para o ingresso do tipo cadeira ${ingresso}!`;
     }
 
+
     if (quantidade > quantidadeIngresso) {
         alert(mensagem);
-        document.getElementById('qtd').value = '';
+        limparQuantidade();
+    }
+    else if (quantidade <= 0) { 
+        alert('A quantidade informada não é válida!');
+        limparQuantidade();
     }
     else {
+        alert('Compra realizada com sucesso!');
         quantidadeIngresso -= quantidade;
         document.getElementById(`qtd-${ingresso}`).textContent = `${quantidadeIngresso}`;
-        document.getElementById('qtd').value = '';
+        limparQuantidade();
     }
+ }
+
+ function limparQuantidade() {
+    document.getElementById('qtd').value = '';
  }
